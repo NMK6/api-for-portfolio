e249a2be347b33f791304a70a4cf268cf888f368
NMK6 <mkehre@gmail.com>
2021-01-03 19:45:52 +0000

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+module.exports = {
+  plugins: {
+    autoprefixer: { browsers: ["last 2 versions"], grid: true },
+  },
+};
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: ["./src/js/index.js", "./src/sass/main.scss"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "css/[name].css",
+            },
+          },
+          {
+            loader: "extract-loader",
+          },
+          {
+            loader: "css-loader?-url",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
