9ec31e0b0f7511feb3a781087620254d26846a58
NMK6 <mkehre@gmail.com>
2021-01-08 17:09:24 +0000

    eslint

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,4 +1,5 @@
 export const elements = {
+  firstScreen: document.querySelector(".first-screen"),
   searchForm: document.querySelector(".search__form"),
   searchInput: document.querySelector(".search__field"),
   searchRecepesList: document.querySelector(".recepes__list"),
